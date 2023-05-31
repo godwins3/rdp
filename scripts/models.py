d65bb60b1528a2f8756041ac9832c6ba3a15b4c7
godwins3 <praisegodwins4@gmail.com>
2023-06-01 02:29:50 +0300

    creating models.py file

---
+++ scripts/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    title = db.Column(db.String(120), nullable=False)
+    slug = db.Column(db.String(250))
+    description = db.Column(db.String(240))
+    body = db.Column(db.Text(), nullable=False)
+    image = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"Post('{ self.id }', '{ self.title }')"
