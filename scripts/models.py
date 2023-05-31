689bed4b699ed7e7da8024fbb73ed78a4e9808cf
godwins3 <praisegodwins4@gmail.com>
2023-06-01 02:48:18 +0300

    adding migrations

--- scripts/models.py
+++ scripts/models.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 
-# from . import db
+from . import db
 
-# class Post(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-#     created_at = db.Column(db.DateTime, default=datetime.now)
-#     title = db.Column(db.String(120), nullable=False)
-#     slug = db.Column(db.String(250))
-#     description = db.Column(db.String(240))
-#     body = db.Column(db.Text(), nullable=False)
-#     image = db.Column(db.String(120))
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    title = db.Column(db.String(120), nullable=False)
+    slug = db.Column(db.String(250))
+    description = db.Column(db.String(240))
+    body = db.Column(db.Text(), nullable=False)
+    image = db.Column(db.String(120))
 
-#     def __repr__(self):
-#         return f"Post('{ self.id }', '{ self.title }')"
+    def __repr__(self):
+        return f"Post('{ self.id }', '{ self.title }')"
